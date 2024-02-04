558db39648052176e33778ea0aba97716ba8c787
mFaYizp <fayizfaz78@gmail.com>
2024-02-04 23:38:03 +0530

    working

--- app/index.js
+++ app/index.js
@@ -1,11 +1,40 @@
-import { Text, View } from "react-native"
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
 
-const Home =()=>{
-return(
-<View>
-    <Text>Home</Text>
-</View>
-)
-}
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  NearbyJobs,
+  PopularJobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
-export default Home
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <PopularJobs />
+          <NearbyJobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
